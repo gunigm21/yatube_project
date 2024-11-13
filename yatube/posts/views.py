709d0d4d9209b08dfeca509fd04a905856157053
Guzel Egorova <gunigm21@gmail.com>
2024-11-13 13:10:48 +0400

    before added model Group

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-    return HttpResponse('Главная страница')
-
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Посты')
+    template = 'posts/group_list.html'
+    text = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'text': text,
+    }
+    return render(request, template, context) 
 
 
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import include, path
 import posts.urls
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', include(posts.urls)),
+    path('', include(posts.urls, namespace='posts')),
     path('admin/', admin.site.urls)
 ]
